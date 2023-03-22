a03826e23b8eec73a10bd9460b83dbf4bb2cc71c
officialsyw <2086451347@qq.com>
2023-03-22 21:36:08 +0800

    Site updated: 2023-03-22 21:36:03

---
+++ js/windows_custom.js
@@ -0,0 +1,16 @@
+function getF12Key() {
+    new Vue({
+        data: function () {
+            this.$notify({
+                title: "你已被发现😜",
+                message: "小伙子，扒源记住要遵循GPL协议！",
+                position: 'top-left',
+                offset: 50,
+                showClose: true,
+                type: "warning",
+                duration: 5000
+            });
+        }
+    })
+}
+
